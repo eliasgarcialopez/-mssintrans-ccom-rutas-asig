bab5009d5be44300f02655e42b2d9553dffdf5c4
esanvicente <erick.sanvicente@people-media.com.mx>
2022-09-01 17:22:19 -0500

    Se corrige el nombre de la tabla de viaticos

--- src/main/java/mx/gob/imss/mssistrans/ccom/rutas/model/Viaticos.java
+++ src/main/java/mx/gob/imss/mssistrans/ccom/rutas/model/Viaticos.java
@@ -9,7 +9,7 @@ import java.time.LocalDate;
 @Getter
 @Setter
 @Entity
-@Table(name = "sintranst_viaticos")
+@Table(name = "SINTRANST_VIATICOS")
 public class Viaticos {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
